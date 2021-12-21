2bbc953f58afa8b13ed5b8d2a387825bfec43c2a
Desblaze <92439363+Desblaze@users.noreply.github.com>
2021-12-21 16:24:04 +0300

    Add files via upload

---
+++ js/1.js
@@ -0,0 +1,14 @@
+"use strict"
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+const square = new Rectangle(10, 15);
+
+console.log(square.calcArea())
